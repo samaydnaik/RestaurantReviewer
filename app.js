fcc838f27ec19b5155c03384e1eb5dfb50317daf
CaptainCode <43927583+CodeLegacy@users.noreply.github.com>
2020-02-16 14:23:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,142 @@
+var express = require("express")
+app =express()
+bodyParser=require("body-parser")
+expressSanitizer=require("express-sanitizer")
+mongoose=require("mongoose")
+methodOverride=require("method-override")
+
+mongoose.connect("mongodb://localhost/restaurant", { useNewUrlParser: true })
+app.set("view engine","ejs")   
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(expressSanitizer())
+app.use(methodOverride("_method"))
+
+var blogSchema=new mongoose.Schema({
+    title:String,
+    image:String,
+    body:String,
+    created:{type: Date,default:Date.now}
+})
+
+var Blog = mongoose.model("Blog",blogSchema) 
+app.get("/",function(req,res){
+    Blog.find({},function(err,blogs){
+        if(err){
+            console.log("ERROR!")
+        }
+        else{
+            res.render("home")
+        }
+    })
+    
+})
+
+app.get("/blogs",function(req,res){
+    if(req.query.search){
+        const regex= new RegExp(escapeRegex(req.query.search),'gi')
+        Blog.find({title: regex}, function (err, blogs) {
+            if (err) {
+                console.log("ERROR!!")
+            }
+            else {
+                res.render("index", { blogs: blogs })
+            }
+        })
+    }else{
+    Blog.find({},function(err,blogs){
+        if(err){
+            console.log("ERROR!!")
+        }
+        else{
+            res.render("index",{blogs:blogs})
+        }
+    })
+}
+})
+app.get("/blogs/login",function(req,res){
+    Blog.find({},function(err,blogs){
+        if(err){
+            console.log("ERROR!!")
+        }
+        else
+        {
+          res.render("login",{blogs:blogs})  
+        }
+    })
+})
+app.get("/blogs/new",function(req,res){
+    res.render("new")
+})
+
+app.post("/blogs",function(req,res){
+     req.body.blog.body = req.sanitize(req.body.blog.body)
+    Blog.create(req.body.blog,function(err,newBlog){
+        if(err)
+        {
+            res.render("new")
+        }
+        else{
+            res.redirect("/blogs")
+        }
+    })
+})
+
+app.get("/blogs/:id",function(req,res){
+    Blog.findById(req.params.id,function(err,foundBlog){
+        if(err)
+        {
+            res.redirect("/blogs")
+        }
+        else
+        {
+            res.render("show",{blog:foundBlog})
+        }
+    })
+})
+
+app.get("/blogs/:id/edit",function(req,res){
+    Blog.findById(req.params.id,function(err,foundBlog){
+        if(err)
+        {
+            res.redirect("/blogs")
+        }
+        else
+        {
+            res.render("edit",{blog:foundBlog})
+        }
+    })
+})
+
+app.put("/blogs/:id",function(req,res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+    Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err,updatedBlog){
+        if(err)
+        {
+            res.redirect("/blogs")
+        }
+        else{
+            res.redirect("/blogs/"+req.params.id)
+        }
+    })
+})
+
+app.delete("/blogs/:id",function(req,res)
+{
+    Blog.findByIdAndRemove(req.params.id,function(err){
+        if(err){
+            res.redirect("/blogs")
+        }
+        else
+        {
+            res.redirect("/blogs")
+        }
+    })
+})
+
+function escapeRegex(text){
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g,"\\s&")
+}
+app.listen(3000,function(){
+    console.log("Server is Running")
+})
